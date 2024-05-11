94b09f4d808ab817e2fbd42b86fc5b4ad3fe9c49
Daniel1991T <dantifui.91@gmail.com>
2024-05-11 22:20:18 +0300

    Refactor Rating page to fetch and display reviews for the authenticated user and fix typescript error.

--- app/(root)/profile/rating/page.tsx
+++ app/(root)/profile/rating/page.tsx
@@ -1,21 +1,23 @@
 import RatingComponent from "@/components/Rating";
 import ReviewCard from "@/components/cards/ReviewCard";
+import { getReviewOfUserClerkId } from "@/lib/actions/rating.actions";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
 const Rating = async () => {
+  const { userId } = auth();
+  if (!userId) return redirect("/");
+  const reviews = await getReviewOfUserClerkId(userId);
   return (
     <div className="flex flex-col w-full  items-center max-w-lg mx-auto justify-center">
       <RatingComponent rate={5} />
       <div className="w-full flex-col flex gap-2">
         <h2 className="text-lg font-semibold">Reviews</h2>
-        <ReviewCard
-          fullName="Tifui Daniel"
-          imageAvatar=""
-          posteAt="12 Dec 2021"
-          review="Nice book"
-          reviewRate={4}
-        />
+        {reviews.map((review, index) => (
+          <ReviewCard key={review._id + index} reviewItem={review} />
+        ))}
       </div>
     </div>
   );
